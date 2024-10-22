4eee1065555205c10106eb75cc0a8acfc32b2a4a
DragoonXVIII <99823997+DragoonXVIII@users.noreply.github.com>
2024-10-22 10:58:15 +0200

    ZPO lab7 1/2

---
+++ V_semestr/ZPO/lab7/main.cpp
@@ -0,0 +1,64 @@
+#include "mainwindow.h"
+
+#include <QApplication>
+#include <QSet>
+#include <QString>
+
+#include <iostream>
+#include <cctype>  // std::tolower
+
+
+
+bool isPangram(const QString& text)
+{
+    QSet<char> uniqueLetters;
+
+    // Przechodzimy przez każdy znak napisu
+    for (const QChar& ch : text)
+    {
+        if (ch.isLetter())
+        {
+            // Konwertujemy na małą literę i dodajemy do zbioru
+            uniqueLetters.insert(std::tolower(ch.toLatin1()));
+        }
+    }
+
+    // Jeśli zbiór ma 26 elementów, oznacza to, że mamy wszystkie litery alfabetu
+    return uniqueLetters.size() == 26;
+}
+
+
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+
+
+    // Przykładowy test dla funkcji isPangram
+    QString test1 = "The quick brown fox jumps over the lazy dog";
+    QString test2 = "Hello World!";
+
+    // Sprawdzanie czy napisy są pangramami
+    if (isPangram(test1))
+    {
+        std::cout << "\"" << test1.toStdString() << "\" jest pangramem." << std::endl;
+    }
+    else
+    {
+        std::cout << "\"" << test1.toStdString() << "\" nie jest pangramem." << std::endl;
+    }
+
+    if (isPangram(test2))
+    {
+        std::cout << "\"" << test2.toStdString() << "\" jest pangramem." << std::endl;
+    }
+    else
+    {
+        std::cout << "\"" << test2.toStdString() << "\" nie jest pangramem." << std::endl;
+    }
+
+
+    return a.exec();
+}
