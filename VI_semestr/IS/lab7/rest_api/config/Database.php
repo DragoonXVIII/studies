d2843ac127df77af802061597f2160c90cbbd34a
DragoonXVIII <99823997+DragoonXVIII@users.noreply.github.com>
2025-03-17 18:24:21 +0100

    IS - lab7(advanced)

--- VI_semestr/IS/lab7/rest_api/config/Database.php
+++ VI_semestr/IS/lab7/rest_api/config/Database.php
@@ -1,10 +1,20 @@
 <?php
 class Database
 {
+    /*
+    // Basic connection
     private $host = 'db';
     private $user = 'root';
     private $password = "elozelo2137";
     private $database = "world";
+    */
+
+    // Advanced connection
+    private $host = 'db';
+    private $user = 'newuser';
+    private $password = 'newpassword'; 
+    private $database = "world";
+
     public function getConnection()
     {
         $conn = new mysqli($this->host, $this->user, $this->password, $this->database);
