560d583a1e8feb30d736ef17f6ab1d5684a26fb5
DragoonXVIII <99823997+DragoonXVIII@users.noreply.github.com>
2025-10-21 14:16:52 +0200

    AiP(.NET) - Lab2

---
+++ VII_semestr/AiP(.NET)/Lab2/Lab2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    
+        class Program
+        {
+            static async Task Main()
+            {
+                UsdCourse.Current = await UsdCourse.GetUsdCourseAsync();
+
+                //Zad 2.1
+                List<Fruit> fruits = new List<Fruit>();
+                for (int i = 0; i < 15; i++)
+                    fruits.Add(Fruit.Create());
+
+                Console.WriteLine("Zad2.2");
+                foreach (var fruit in fruits)
+                    Console.WriteLine(fruit);
+
+                Console.WriteLine("\nZad2.3");
+                Console.WriteLine("\nZad2.6");
+                var sweetFruits = fruits
+                    .Where(f => f.IsSweet)
+                    .OrderByDescending(f => f.Price);
+
+                foreach (var fruit in sweetFruits)
+                    Console.WriteLine(fruit);
+            }
+        }
+}
