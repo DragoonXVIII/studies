74bd390b9bbd78ca5385fd3afcb29303688352d6
DragoonXVIII <99823997+DragoonXVIII@users.noreply.github.com>
2025-11-17 09:51:15 +0100

    ZP(Java) - lab10 (1.6/2)

--- VII_semestr/ZP(Java)/lab10/src/main/java/org/example/Main.java
+++ VII_semestr/ZP(Java)/lab10/src/main/java/org/example/Main.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        StaffScraper.run("https://cs.pollub.pl/staff/");
+        ScraperPracownikow.run("https://cs.pollub.pl/staff/");
         //DeanRectorSearch.run("https://weii.pollub.pl/wyszukiwarka?query=Dni+wolne");
     }
 }
